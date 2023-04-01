d38653f10f3e7aca7d599fd20b26c5f5bd4d20e5
Taofiq-Bakare <taofiq01@gmail.com>
2023-04-01 14:08:08 +0000

    changed the file structure

--- application.py
+++
@@ -1,6 +0,0 @@
-from fsphinx import *
-from cloudmining import CloudMiningApp
-app = CloudMiningApp()
-cl = FSphinxClient.FromConfig('sphinx_client.py')
-app.set_fsphinx_client(cl)
-app.run()
--- cloudmining/application.py
+++ cloudmining/application.py
@@ -3,5 +3,6 @@ from cloudmining import CloudMiningApp
 
 app = CloudMiningApp()
 cl = FSphinxClient.FromConfig('sphinx_client.py')
+app.set_port(80)
 app.set_fsphinx_client(cl)
 app.run()
