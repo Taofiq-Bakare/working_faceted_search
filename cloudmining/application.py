38e588530b66246d54f566339093442fb9dcff01
Taofiq-Bakare <taofiq01@gmail.com>
2023-04-08 15:30:01 +0000

    Fix: refactored the start app bash script

--- cloudmining/application.py
+++ cloudmining/application.py
@@ -1,7 +1,17 @@
 from fsphinx import *
 from cloudmining import CloudMiningApp
 
-app = CloudMiningApp()
-cl = FSphinxClient.FromConfig('sphinx_client.py')
-app.set_fsphinx_client(cl)
-app.run()
+# app = CloudMiningApp()
+# cl = FSphinxClient.FromConfig('sphinx_client.py')
+# app.set_fsphinx_client(cl)
+# app.run()
+
+
+def main():
+    app = CloudMiningApp()
+    cl = FSphinxClient.FromConfig('sphinx_client.py')
+    app.set_fsphinx_client(cl)
+    app.run()
+
+if __name__ == '__main__':
+    main()
--- start_application.sh
+++ start_application.sh
@@ -9,20 +9,6 @@ sudo searchd -c ./config/sphinx_indexer.cfg
 cp ./config/sphinx_client.py ../../cloudmining/.
 
 cd ../../cloudmining
-file="application.py"
-[[ -f "$file" ]] && rm -f "$file"
-cat <<EOT >> $file
-from fsphinx import *
-from cloudmining import CloudMiningApp
 
-app = CloudMiningApp()
-cl = FSphinxClient.FromConfig('sphinx_client.py')
-app.set_fsphinx_client(cl)
-app.run()
-EOT
-
-echo
-echo  "#### DONE ###"
-echo
 
 python application.py
