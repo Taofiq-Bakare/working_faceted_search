f8cf64d1525d54e6bd24d227c40d2c5108edfa45
Taofiq-Bakare <taofiq01@gmail.com>
2023-04-08 14:55:28 +0000

    Fix: the app works using a shell script

--- cloudmining/application.py
+++ cloudmining/application.py
@@ -2,6 +2,6 @@ from fsphinx import *
 from cloudmining import CloudMiningApp
 
 app = CloudMiningApp()
-cl = FSphinxClient.FromConfig('cloudmining/sphinx_client.py')
+cl = FSphinxClient.FromConfig('sphinx_client.py')
 app.set_fsphinx_client(cl)
 app.run()
